f8d188c06869e2029c2cde4a48956ca8ff83e66f
Simran Singh <simran.singh@bounteous.com>
2026-02-18 15:54:46 +0530

    Solved first missing positive

---
+++ first_missing_positive.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            int idx = nums[i] - 1;
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[idx]) {
+                int temp = nums[i];
+                nums[i] = nums[idx];
+                nums[idx] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+
+
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=j+1) {
+                return j+1;
+            }
+        }
+        return nums.length+1;
+    }
+}
